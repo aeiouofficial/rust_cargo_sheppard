7e8cf5c85adc12f17cbcb5b23fb5afb09bf599f5
aeiou <bigbred1992@googlemail.com>
2026-05-03 17:47:08 +0200

    Delete shepherd_init.bat

--- shepherd_init.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-setlocal
-cd /d "%~dp0"
-REM Wrapper for the location-agnostic release script
-powershell -ExecutionPolicy Bypass -File ".\shepherd_init.ps1"
-if %errorlevel% neq 0 pause
-endlocal
